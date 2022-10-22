993cbca5fc06d6321a0824e874ef63968e3011ce
haris400008 <59230800+haris400008@users.noreply.github.com>
2022-10-22 22:41:54 +0500

    Add files via upload

---
+++ Tic Tac Toe/app.js
@@ -0,0 +1,80 @@
+const buttons = {
+    b1: document.getElementById("b1"),
+    b2: document.getElementById("b2"),
+    b3: document.getElementById("b3"),
+    b4: document.getElementById("b4"),
+    b5: document.getElementById("b5"),
+    b6: document.getElementById("b6"),
+    b7: document.getElementById("b7"),
+    b8: document.getElementById("b8"),
+    b9: document.getElementById("b9"),
+};
+
+let currentPlayer = 1;
+
+const checkProgress = () => {
+    if(buttons.b1.innerText && buttons.b1.innerText === buttons.b2.innerText && 
+        buttons.b2.innerText === buttons.b3.innerText){
+            console.log(`Player ${buttons.b1.innerText} Won`);
+            return reset();
+    } else if(buttons.b4.innerText && buttons.b4.innerText === buttons.b5.innerText && 
+        buttons.b5.innerText === buttons.b6.innerText){
+            console.log(`Player ${buttons.b4.innerText} Won`);
+            return reset();
+    }else if(buttons.b7.innerText && buttons.b7.innerText === buttons.b8.innerText && 
+        buttons.b8.innerText === buttons.b9.innerText){
+            console.log(`Player ${buttons.b7.innerText} Won`);
+            return reset();
+    } else if(buttons.b1.innerText && buttons.b1.innerText === buttons.b4.innerText && 
+        buttons.b4.innerText === buttons.b7.innerText){
+            console.log(`Player ${buttons.b1.innerText} Won`);
+            return reset();
+    }else if(buttons.b2.innerText && buttons.b2.innerText === buttons.b5.innerText && 
+        buttons.b5.innerText === buttons.b8.innerText){
+            console.log(`Player ${buttons.b2.innerText} Won`);
+            return reset();
+    }else if(buttons.b3.innerText && buttons.b3.innerText === buttons.b6.innerText && 
+        buttons.b6.innerText === buttons.b9.innerText){
+            console.log(`Player ${buttons.b3.innerText} Won`);
+            return reset();
+    } else if(buttons.b1.innerText && buttons.b1.innerText === buttons.b5.innerText && 
+        buttons.b5.innerText === buttons.b9.innerText){
+            console.log(`Player ${buttons.b1.innerText} Won`);
+            return reset();
+    } else if(buttons.b3.innerText && buttons.b3.innerText === buttons.b5.innerText && 
+        buttons.b5.innerText === buttons.b7.innerText){
+            console.log(`Player ${buttons.b3.innerText} Won`);
+            return reset();
+    }
+    for (const key in buttons){
+        const button = buttons[key];
+        if (button.innerText === ""){
+            return;
+        }
+    }
+    console.log("Drawn");
+    return reset();
+};
+
+const play = (btn) => {
+    const button = buttons[btn];
+    if(currentPlayer === 1){
+        button.innerText = "x";
+        currentPlayer = 2; 
+    }
+    else{
+        button.innerText = "o";
+        currentPlayer = 1;
+    }
+    button.disabled = true;
+    checkProgress();
+};
+
+const reset = () => {
+    for(const key in  buttons){
+        const button = buttons[key];
+        button.disabled = false;
+        button.innerText = "";
+    }
+    currentPlayer = 1;
+};
